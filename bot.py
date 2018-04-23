5b76707f2e0bc3c4fa1e0e446f42363009f40433
Tiger Zhang <tiger.c.zhang@gmail.com>
2018-04-23 01:18:56 -0500

    Added help command

--- bot.py
+++ bot.py
@@ -2,9 +2,33 @@ import telegram
 import telegram.ext as ext
 import logging
 import tweepy
+import ConfigParser
 
+# TODO: parse keys from config file instead of API key.txt
+
+# dictionary of all chats and what accounts to track
+chat_map = dict()
 # list of followers to keep track of (5 max?)
 follow_list = []
+
+# store api keys externally (not on git)
+keys = open("API key.txt", "r")
+
+# get telegram keys
+telegram_key = keys.readline().strip()
+telegram_bot = telegram.Bot(telegram_key)
+print(telegram_bot.get_me())
+
+# get keys for twitter
+consumer_key = keys.readline().strip()
+consumer_secret = keys.readline().strip()
+access_token = keys.readline().strip()
+access_secret = keys.readline().strip()
+
+# authorize twitter api usage
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+twitter = tweepy.API(auth)
 
 def main():
     # set up logging
@@ -12,34 +36,13 @@ def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)2 - %(message)s', 
         level=logging.INFO)
     '''
-
-    # store api keys externally (not on git)
-    keys = open("API key.txt", "r")
-
-    # get telegram keys
-    telegram_key = keys.readline().strip()
-    telegram_bot = telegram.Bot(telegram_key)
-    print(telegram_bot.get_me())
-
-    # get keys for twitter
-    consumer_key = keys.readline().strip()
-    consumer_secret = keys.readline().strip()
-    access_token = keys.readline().strip()
-    access_secret = keys.readline().strip()
-
-    # authorize twitter api usage
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
-    twitter = tweepy.API(auth)
-
-    # reads name of user to follow from command line
     '''
+    # reads name of user to follow from command line
     username = raw_input("User to follow: ")
     print(twitter.get_user(username).id)
     follow_list = [str(twitter.get_user(username).id)]
     print(follow_list)
     '''
-
     # telegram bot stuff
     updater = ext.Updater(bot = telegram_bot)
     dispatcher = updater.dispatcher
@@ -66,15 +69,20 @@ def main():
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Hello! I am Feh Bot, and I follow Twitter accounts!")
 
-# TODO: check if account is a valid twitter account
+
+# fafds45132 is a invalid account
 def add(bot, update, args):
     new_user = args[0]
     output_text = ""
-    if(follow_list.count(new_user) < 1):
+    if(follow_list.count(new_user) >= 1):
         follow_list.append(args[0])
-        output_text = "Now following " + new_user + "."
-    else:
         output_text = "Already following " + new_user + "."
+    else:
+        try:
+            twitter.get_user(new_user)
+            output_text = "Now following " + new_user + "."
+        except tweepy.error.TweepError:
+            output_text = "Error: " + new_user + " is not a valid Twitter account"
     bot.send_message(chat_id=update.message.chat_id, text=output_text)
 
 def list_followed(bot, update):
@@ -85,6 +93,13 @@ def list_followed(bot, update):
         else:
             output_text += follow_list[index] + "\n"
     bot.send_message(chat_id=update.message.chat_id, text=output_text)
+
+def help(bot, update):
+    help_text = "Hello, I am feh_bot! I listen to Twitter accounts and post to chats when an account makes a tweet\n" \
+                "/start - starts the bot in this chat\n" \
+                "/add USERNAME - adds USERNAME to the follow list\n" \
+                "/list_followed - lists out which Twitter accounts I am monitoring for updates"
+    bot.send_message(chat_id=update.message.chat_id, text=help_text)
 
 # Listener for Twitter, overrides tweepy's StreamListener to provide 
 # functionality for telegram
